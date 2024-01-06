63a90780a1490c89d466aeec34ae8a657197dd27
irgendwieCool <150230013+irgendwieCool@users.noreply.github.com>
2024-01-06 15:45:26 +0100

    temp

---
+++ Backend/src/main/java/com/ppj/backend/Webservice/Token.java
@@ -0,0 +1,21 @@
+package com.ppj.backend.Webservice;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+public class Token
+{
+
+    
+}
+
+     
+    
+
+
