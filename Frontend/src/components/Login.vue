b0532b7030fe830409ab31380a622094d5b8a659
PDCool64 <p.d.cool@web.de>
2024-01-08 01:16:26 +0100

    first login attempt

--- Frontend/src/components/Login.vue
+++ Frontend/src/components/Login.vue
@@ -1,9 +1,35 @@
+<script setup>
+
+import { ref } from 'vue';
+    const email = ref('');
+    const password = ref('');
+
+    const submitForm = async () => {
+        const response = await fetch('http://localhost:8080/Backend/login/' + email.value, {
+            method: 'POST',
+            headers: {
+                'password': password.value,
+            },
+        });
+
+        if (response.status === 401) {
+            errorMessage.value = "Die Email oder das Passwort ist falsch.";
+            error.value = true;
+        } else {
+            const data = await response.json();
+            console.log(data);
+            errorMessage.value = "Registrierung erfolgreich.";
+            error.value = false;
+        }
+    };
+</script>
+
 <template>
     <div class="login form">
         <h1>Login</h1>
-        <form action="/auth" method="post">
-            <input type="text" id="email" name="email" placeholder="Email" required>
-            <input type="password" id="password" name="password" placeholder="Password" required>
+        <form action="localhost:8080/Backend/login" method="post" @submit.prevent="submitForm">
+            <input v-model="email" type="text" id="email" name="email" placeholder="Email" required>
+            <input v-model="password" type="password" id="password" name="password" placeholder="Password" required>
             <button type="submit">Login</button>
         </form>
         <p>Don't have an account? <RouterLink to="/registration">Register</RouterLink></p>
