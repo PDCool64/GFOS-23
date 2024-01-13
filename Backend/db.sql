8c2c8e493a97c8c4c4d7cf1b753b21ee7781dba3
PDCool64 <p.d.cool@web.de>
2024-01-13 19:09:02 +0100

    initial

--- Backend/db.sql
+++ Backend/db.sql
@@ -1,44 +1,62 @@
-CREATE TABLE Account(
-    name VARCHAR(50),
-    vorname VARCHAR(50),
-    personalnummer INT UNIQUE,
-    geburtsdatum DATE,
-    wochenstunden INT,
-    email VARCHAR(255) UNIQUE,
-    passwortHash VARCHAR(255) UNIQUE,
-    isAdmin BOOLEAN,
-    token VARCHAR(64),
-    timestampLetzteAktion TIMESTAMP,
-    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)
-);
-CREATE TABLE Meldung(
-    beginTimestamp TIMESTAMP,
-    endTimestamp TIMESTAMP,
-    grund VARCHAR(255),
-    kommentar VARCHAR(255),
-    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-    account INT NOT NULL,
-    FOREIGN KEY(account) REFERENCES Account(id) ON DELETE CASCADE
-);
-CREATE TABLE Kurs(
-    bezeichnung VARCHAR(20),
-    checkinCode VARCHAR(5),
-    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-    leiter INT NOT NULL,
-    FOREIGN KEY(leiter) REFERENCES Account(id) ON DELETE CASCADE
-);
-CREATE TABLE Unterricht(
-    beginzeit TIME,
-    endzeit TIME,
-    wochentag VARCHAR(10),
-    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
-    kurs INT NOT NULL,
-    FOREIGN KEY(kurs) REFERENCES Kurs(id) ON DELETE CASCADE
-);
-CREATE TABLE Teilnahme(
-    account INT NOT NULL,
-    kurs INT NOT NULL,
-    PRIMARY KEY(account, kurs),
-    FOREIGN KEY(account) REFERENCES Account(id) ON DELETE CASCADE,
-    FOREIGN KEY(kurs) REFERENCES Kurs(id) ON DELETE CASCADE
-);
+CREATE TABLE Account (
+    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 0, INCREMENT BY 1),
+    email VARCHAR(255) NOT NULL,
+    vorname VARCHAR(64) NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    geburtsdatum DATE NOT NULL,
+    passwortHash VARCHAR(128) NOT NULL, 
+    isAdmin BOOLEAN NOT NULL DEFAULT false 
+)
+
+CREATE TABLE Kurs (
+    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 0, INCREMENT BY 1),
+    fach VARCHAR(64) NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    stufe INT NOT NULL, 
+    leiterID INT,     
+    FOREIGN KEY (leiterID) REFERENCES Account(id)
+)
+
+CREATE TABLE KursTeilnahme (
+    accountID INT NOT NULL,
+    kursID INT NOT NULL,
+    PRIMARY KEY (accountID, kursID),
+    FOREIGN KEY (accountID) REFERENCES Account(id),
+    FOREIGN KEY (kursID) REFERENCES Kurs(id)
+)
+
+CREATE TABLE Unterricht (
+    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 0, INCREMENT BY 1),
+    kursID INT NOT NULL,
+    beginZeit VARCHAR(5) NOT NULL,
+    endZeit VARCHAR(5) NOT NULL,
+    FOREIGN KEY (kursID) REFERENCES Kurs(id)
+)
+
+CREATE TABLE Stunde (
+    id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 0, INCREMENT BY 1),
+    unterrichtID INT NOT NULL,
+    beginTimestamp TIMESTAMP NOT NULL,
+    endTimestamp TIMESTAMP NOT NULL,
+    checkInCode VARCHAR(6) NOT NULL,
+    FOREIGN KEY (unterrichtID) REFERENCES Unterricht(id)
+)
+
+CREATE TABLE StundeTeilnahme (
+    accountID INT NOT NULL,
+    stundeID INT NOT NULL,
+    anwesend BOOLEAN NOT NULL DEFAULT false,
+    note INT,
+    PRIMARY KEY (accountID, stundeID),
+    FOREIGN KEY (accountID) REFERENCES Account(id),
+    FOREIGN KEY (stundeID) REFERENCES Stunde(id)
+)
+
+CREATE TABLE StundeLeitung (
+    accountID INT NOT NULL,
+    stundeID INT NOT NULL,
+    leitungsBewertung INT NOT NULL,
+    PRIMARY KEY (accountID, stundeID),
+    FOREIGN KEY (accountID) REFERENCES Account(id),
+    FOREIGN KEY (stundeID) REFERENCES Stunde(id)
+)
