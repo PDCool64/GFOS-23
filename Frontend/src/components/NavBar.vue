97df4ec62a7cd47277a0617c1b11c19070604946
irgendwieCool <150230013+irgendwieCool@users.noreply.github.com>
2024-02-04 10:32:07 +0100

    changed NavBar

--- Frontend/src/components/NavBar.vue
+++ Frontend/src/components/NavBar.vue
@@ -1,134 +1,115 @@
 <template>
-	<nav>
-		<ul class="große-liste">
-			<li class="große-liste"><router-link to="/">Home</router-link></li>
-			<li class="große-liste">
-				<router-link to="/stundenplan">Calendar</router-link>
-			</li>
-		</ul>
-		<div
-			class="klickbarer-bereich"
-			@click="listeSichtbar = !listeSichtbar"
-		>
-			<img
-				src="../assets/pictures/unnamed.png"
-				alt="Bild kann nicht geladen werden"
-				style="width: 40px; height: 40px"
-			/>
-			<ul v-if="listeSichtbar" class="kleine-liste">
-				<li><router-link to="/"> Home</router-link></li>
-				<li><router-link to="/profile"> Profile</router-link></li>
-			</ul>
-		</div>
-	</nav>
+  <div>
+    <nav class="navbar">
+      <!-- Hier kommen die Inhalte deiner Navbar -->
+      <ul>
+        <li>
+          <a href="/">Home</a></li>
+        <li><a href="/profile">Profile</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+      <img src="./unnamed.png" alt="Ende der Navbar" class="navbar-image" @click="toggleTable" />
+    </nav>
+    <ol v-show="showList" class="info-list">
+        <li><a href="/">Home</a></li>
+        <li><a href="/profile">Profile</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ol>
+      </div>
 </template>
 
-<script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
-
-const listeSichtbar = ref(false);
-
-const closeListe = (event) => {
-  if (event.target.closest('.klickbarer-bereich')) return;
-  listeSichtbar.value = false;
+<script>
+export default {
+  data() {
+    return {
+      showList: false // Variable, um den Listenstatus zu verfolgen
+    };
+  },
+  methods: {
+    toggleTable() {
+      this.showList = !this.showList; // Umkehrung des Listenstatus
+    }
+  }
 };
 
-onMounted(() => {
-  window.addEventListener('click', closeListe);
-});
-
-onUnmounted(() => {
-  window.removeEventListener('click', closeListe);
-});
 </script>
 
 <style scoped>
-nav {
-	background-image: url("../assets/pictures/blauer Hintergrund.jpg");
-	background-size: 100% 100%;
-	padding: 10px;
-	color: #6b8c83;
-	display: flex;
-	justify-content: center;
-	box-shadow: 0px 10px 10px var(--shadow);
-	margin: 10px;
-	width: 100%;
-	align-items: center;
-}
-.große-liste {
-	list-style-type: none;
-	margin: 0;
-	padding: 0;
-	display: flex;
-	width: 100%;
-	height: 100%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+/* Hier kommt das Styling für deine Navbar */
+.navbar {
+  background-color: var(--first-color); /* Hintergrundfarbe deiner Navbar */
+  height: 6.25vp; /* 1/8 der gesamten vertikalen Höhe */
+  width: 100%; /* Volle Breite des Bildschirms */
+  position: fixed; /* Fixierte Position am oberen Bildschirmrand */
+  top: 0; /* Oben ausrichten */
+  left: 0; /* Links ausrichten */
+  display: flex;
+  align-items: center;
+  justify-content: space-between; /* Gleichmäßige Verteilung der Elemente */
+  padding: 12px 20px; /* Innenabstand für die Inhalte der Navbar */
+  text-align: center; /* Zentrierung des Inhalts innerhalb der Navbar */
 }
 
-.große-liste li {
-	margin: 0 5px;
-	flex-grow: 1;
-	text-align: center;
-	display: inline-block; /* Listeneinträge nebeneinander */
-	margin-right: 20px; /* Abstand zwischen den Listeneinträgen */
-	color: white;
-	position: relative;
-}
-.große-liste li::after {
-	content: "";
-	position: absolute;
-	right: 0;
-	top: 50%; /* Move the top edge of the line to the vertical center of the list item */
-	width: 1px; /* Width of the line */
-	height: 80%; /* Height of the line */
-	background-color: rgba(0, 0, 0, 0.5); /* Color of the line with 50% opacity */
-	transform: translateY(-50%); /* Shift the line up by half its height */
-}
-a {
-	color: #000000;
-	text-decoration: none;
-	display: block;
-	width: 100%;
-	height: 100%;
-	border-radius: 5px;
-}
-.klickbarer-bereich {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 50px;
-	height: 38px;
-	position: relative;
+/* Styling für die Navbar-Links */
+.navbar ul {
+  list-style: none;
+  display: flex;
+  margin: 0; /* Kein zusätzlicher Abstand für die Liste */
+  padding: 0; /* Kein zusätzlicher Abstand für die Liste */
 }
 
-.kleine-liste {
-	background-color: var(--drop-down-color); /* Hintergrundfarbe in Grauton */
-	border: 1px solid #ccc;
-	border-radius: 5px;
-	position: absolute;
-	top: 100%;
-	box-shadow: 0 2px 5px var(--shadow); 
-	box-sizing: border-box;
-	padding: 0;
-	margin-top: 5px;
+.navbar ul li {
+  flex: 1; /* Gleichmäßige Verteilung der li-Elemente */
+  margin: 0 50px; /* Abstand zwischen den Elementen */
+  display: flex; 
+  align-items: center;
 }
 
-.kleine-liste li:last-child {
-	border-bottom: none; /* Entfernt die untere Grenze des letzten Elements */
+.navbar ul li a {
+  text-decoration: none;
+  color: var(--black); /* Textfarbe der Navbar-Links */
+  font-weight: bold;
+}
+/* Hier kannst du weiteres Styling für deine Seite hinzufügen */
+.main-content {
+  /* Hier kommt das Styling für den Hauptinhalt deiner Seite */
+  margin-top: 12.5vh; /* Platz unterhalb der Navbar */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 87.5vh; /* Der Rest der vertikalen Höhe */
+}
+.navbar-image {
+  max-height: 100%; /* Maximale Höhe des Bildes */
+  width: 40px;
+  height: 40px;
+  border: 2px solid black; /* Schwarze Umrandung */
+  border-radius: 50%; /* Runde Ecken für das runde Bild */
+  cursor: pointer;
 }
 
-.kleine-liste li:hover {
-	background-color: #ddd; /* Hintergrundfarbe beim Hovern für bessere Sichtbarkeit */
-}
-.kleine-liste li {
-	display: block;
-	padding: 5px;
-	color: #cfcfcf;
-	text-align: center;
+.info-list {
+  position: fixed; /* Fixierte Position am rechten Bildschirmrand */
+  top: calc(-0.1vw + 64.4px); /* 10% der Bildschirmhöhe */
+  right: 0; /* Rechts ausrichten */
+  left: auto; /* Automatische Positionierung von links entfernen */
+  bottom: 0; /* Bis zum unteren Rand ausrichten */
+  width: 10%; /* 20% der Bildschirmbreite */
+  min-height: 100; /* Volle Bildschirmhöhe */
+  background-color: var(--second-color); /* Hintergrundfarbe der Liste */
+  padding: 10px; /* Innenabstand für die Inhalte der Liste */
+  display: block; /* Jetzt standardmäßig sichtbar (zum Testen) */
+  list-style: none; /* Entferne die Standardpunktliste */
 }
 
-.klickbarer-bereich {
+.info-list li a {
+  color: var(--black);
+  font-weight: bold;
+}
+.info-list li{
+  margin-bottom: 50px; /* Abstand zwischen den Listeneinträgen */
+
 }
 </style>
