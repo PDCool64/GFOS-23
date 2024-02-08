7f18196b7b8d82f9d5816b4f4df8899118614af8
irgendwieCool <150230013+irgendwieCool@users.noreply.github.com>
2024-02-08 22:18:38 +0100

    cool oder?

---
+++ Frontend/src/components/Anwesenheitsliste.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class ="container">
+    <h1 class="überschrift">Ihre Überschrift hier</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+.container {
+  height: 100vh;
+  display: flex;
+  justify-content: center; /* Überschrift mittig ausrichten */
+  align-items: center; /* Zentrierung vertikal */
+}
+
+.überschrift {
+  text-align: center;
+  background-color: var(--first-color); /* Hintergrundfarbe */
+  padding: 10px; /* Abstand um den Text */
+  border-radius: 10px; /* Abgerundete Ecken */
+}
+</style>
--- Frontend/src/views/AboutView.vue
+++ Frontend/src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import Login from '@/components/Login.vue'
+  import Anwesenheitsliste from '@/components/Anwesenheitsliste.vue'
 </script>
 
 <template>
