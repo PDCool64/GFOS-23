95171816754d7bddfe4e6722ba50ed7a719baeca
PDCool64 <p.d.cool@web.de>
2024-02-14 23:08:09 +0100

    kleiner commit

--- Frontend/src/views/DeleteUnterrichtView.vue
+++ Frontend/src/views/DeleteUnterrichtView.vue
@@ -1,12 +1,10 @@
 <template>
-
-    <div class="container">
-        <h1>Unterricht löschen</h1>
-        <form @submit.prevent="deleteUnterricht">
-            <button type="submit">Löschen</button>
-        </form>
-    </div>
-
+	<div class="wrapper">
+		<h1>Unterricht löschen</h1>
+		<form @submit.prevent="deleteUnterricht">
+			<button type="submit">Löschen</button>
+		</form>
+	</div>
 </template>
 
 <script setup>
@@ -18,10 +16,22 @@ const route = useRoute();
 const kursId = route.params.kurs;
 
 const userData = useUserStore();
-
 </script>
 
 <style scoped>
+@import "../assets/shared_styles/chooser.css";
+
+.wrapper {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
+	width: 100%;
+}
 
 
+button {
+    margin-top: 20px;
+}
 </style>
